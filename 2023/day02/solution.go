93dc7e99bd98fcb5f1dd9d316e45c44d57bce848
Joseph Hastings <jwhastings21@gmail.com>
2023-12-28 12:53:26 -0500

    refactor a bit

--- 2023/day02/solution.go
+++ 2023/day02/solution.go
@@ -1,3 +1,7 @@
+// https://adventofcode.com/2023/day/2
+// A game (one line of input) consists of one or more rounds (delimited by semicolons) that consist
+// of a single handful (delimited by commas)
+
 package main
 
 import (
@@ -10,11 +14,16 @@ import (
 	"strings"
 )
 
-const red int = 12
-const green int = 13
-const blue int = 14
+const (
+	red   = 12
+	green = 13
+	blue  = 14
+)
 
+// ParseGame takes a string with a game number/round results and returns the game number (if valid, 0 otherwise)
+// and the power of the set of minimum cubes for the game to have occurred
 func ParseGame(game string) (gameNum, power int) {
+	// extract the game identifier and round results
 	gameText := game[:strings.Index(game, ":")]
 	roundText := game[strings.Index(game, ":")+2:]
 
@@ -23,60 +32,83 @@ func ParseGame(game string) (gameNum, power int) {
 		panic(err)
 	}
 
-	validGame, power := ParseRound(roundText)
+	validGame, power := ParseRounds(roundText)
 	if !validGame {
 		gameNum = 0
 	}
-
 	return gameNum, power
 }
 
-func ParseRound(rounds string) (valid bool, power int) {
+// ParseRounds takes a string with round results and returns whether the game was possible
+// and the power of the set of minimum cubes needed for the game to have occurred
+func ParseRounds(rounds string) (valid bool, power int) {
 	valid = true // default to a valid round
-	var redCounts []int
-	var greenCounts []int
-	var blueCounts []int
+	var allCounts [3][]int
 
 	roundSplit := strings.Split(rounds, ";")
-	// for each round
+	// for each round in a game
 	for _, round := range roundSplit {
-		cubeSplit := strings.Split(round, ",")
-		// for each colored cube
-		for _, cubeText := range cubeSplit {
-			cubeText = strings.TrimSpace(cubeText)
-
-			cubeCount, err := strconv.Atoi(cubeText[:strings.Index(cubeText, " ")])
-			if err != nil {
-				panic(err)
-			}
-			cubeColor := cubeText[strings.Index(cubeText, " ")+1:]
-
-			switch cubeColor {
-			case "red":
-				redCounts = append(redCounts, cubeCount)
-				if cubeCount > red {
-					valid = false
-				}
-			case "green":
-				greenCounts = append(greenCounts, cubeCount)
-				if cubeCount > green {
-					valid = false
-				}
-			case "blue":
-				blueCounts = append(blueCounts, cubeCount)
-				if cubeCount > blue {
-					valid = false
-				}
+		validHandful, handfulCounts := ParseHandful(round)
+		// set round to invalid if any handful is impossible given cube constraints
+		if !validHandful {
+			valid = false
+		}
+		// append color count to respective slice if positive
+		for i := 0; i < 3; i++ {
+			if handfulCounts[i] > 0 {
+				allCounts[i] = append(allCounts[i], handfulCounts[i])
 			}
 		}
 	}
-
-	maxRed := slices.Max(redCounts)
-	maxGreen := slices.Max(greenCounts)
-	maxBlue := slices.Max(blueCounts)
-	power = maxRed * maxGreen * maxBlue
-
+	power = CalcPower(allCounts)
 	return valid, power
+}
+
+// ParseHandful takes a string with handful results and returns whether it was possible
+// and the counts for each red, green, and blue cube
+func ParseHandful(handful string) (valid bool, counts [3]int) {
+	valid = true                             // default to a valid round
+	cubeSplit := strings.Split(handful, ",") // split handful into the different colored cubes and their amount pulled
+
+	// for each colored cube
+	for _, cubeText := range cubeSplit {
+		cubeText = strings.TrimSpace(cubeText)
+
+		// parse cube color and count as an integer
+		cubeColor := cubeText[strings.Index(cubeText, " ")+1:]
+		cubeCount, err := strconv.Atoi(cubeText[:strings.Index(cubeText, " ")])
+		if err != nil {
+			panic(err)
+		}
+
+		switch cubeColor {
+		case "red":
+			counts[0] = cubeCount
+			if cubeCount > red {
+				valid = false
+			}
+		case "green":
+			counts[1] = cubeCount
+			if cubeCount > green {
+				valid = false
+			}
+		case "blue":
+			counts[2] = cubeCount
+			if cubeCount > blue {
+				valid = false
+			}
+		}
+	}
+	return valid, counts
+}
+
+// CalcPower takes the counts of cubes by color and returns the product of their maximum color counts
+func CalcPower(counts [3][]int) (power int) {
+	power = 1
+	for i := 0; i < 3; i++ {
+		power *= slices.Max(counts[i])
+	}
+	return power
 }
 
 func main() {
