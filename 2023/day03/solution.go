39c81e1d67635a3f3fc8530f806e2b3683a6ad7a
Joseph Hastings <jwhastings21@gmail.com>
2023-12-28 19:37:04 -0500

    solution part two (ugly but correct)

--- 2023/day03/solution.go
+++ 2023/day03/solution.go
@@ -12,38 +12,30 @@ import (
 
 const (
 	width      int    = 140
-	digits     string = "0123456789"
 	notsymbols string = ".0123456789"
 )
 
-func AppendNewToSlice(slice []string, char string) []string {
-	if IsSymbol(char) {
-		if len(slice) == 0 {
-			return append(slice, char)
-		}
-		for _, ele := range slice {
-			if ele == char {
-				return slice
-			}
-		}
-		return append(slice, char)
+type gear struct {
+	a int
+	b int
+}
+
+func IsSymbol(char string) (isSymbol bool, symbol string) {
+	if !(strings.ContainsAny(char, notsymbols)) {
+		return true, char
 	}
-	return slice
+	return false, ""
 }
 
-func IsSymbol(char string) bool {
-	return !(strings.ContainsAny(char, notsymbols))
-}
-
-func CheckRange(a, b int, line string) bool {
+func CheckRange(a, b, row int, line string) (adjacent bool, symbol string, loc gear) {
 	for i := a; i < b; i++ {
 		char := string(line[i])
-		if IsSymbol(char) {
-			return true
+		symbolCheck, symbol := IsSymbol(char)
+		if symbolCheck {
+			return true, symbol, gear{i, row}
 		}
 	}
-	return false
-
+	return false, "", gear{-1, -1}
 }
 
 func main() {
@@ -55,22 +47,22 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	var schematic []string
-	var symbols []string
+	// var symbols []string
 	for scanner.Scan() {
 		// get string from input file, line by line, append to slice
 		stringValue := scanner.Text()
 		schematic = append(schematic, stringValue)
 		// iterate over line to identify new symbols
-		for _, char := range stringValue {
-			symbols = AppendNewToSlice(symbols, string(char))
-		}
+		// for _, char := range stringValue {
+		// 	symbols = AppendNewToSlice(symbols, string(char))
+		// }
 	}
 	file.Close()
 
 	var partNumbers []int
 	var adjacentSymbol bool
+	var gearTable = make(map[gear][]string)
 	for row := 0; row < len(schematic); row++ {
-		// for row := 0; row < 3; row++ {
 		line := schematic[row]
 		re := regexp.MustCompile("[0-9]+")
 		allDigits := re.FindAllIndex([]byte(line), -1)
@@ -80,44 +72,105 @@ func main() {
 			a := ab[0] // index for start of number
 			b := ab[1] // index of end of number + 1
 			numStr = line[a:b]
-			// check horizontals
+
 			charToLeft := (a > 0)
 			charToRight := (b < width)
 			charAbove := row > 0
 			charBelow := row < len(schematic)-1
 
-			if charToLeft && CheckRange(a-1, a, line) {
-				adjacentSymbol = true
+			// check horizontals
+			if charToLeft {
+				adjacent, symbol, location := CheckRange(a-1, a, row, line)
+				if adjacent {
+					adjacentSymbol = true
+					if symbol == "*" {
+						gearTable[location] = append(gearTable[location], numStr)
+					}
+				}
 			}
-			if charToRight && CheckRange(b, b+1, line) {
-				adjacentSymbol = true
+			if charToRight {
+				adjacent, symbol, location := CheckRange(b, b+1, row, line)
+				if adjacent {
+					adjacentSymbol = true
+					if symbol == "*" {
+						gearTable[location] = append(gearTable[location], numStr)
+					}
+				}
 			}
 			// check above
 			if charAbove {
 				lineAbove := schematic[row-1]
 
-				if charToLeft && charToRight && CheckRange(a-1, b+1, lineAbove) {
-					adjacentSymbol = true
-				} else if charToLeft && CheckRange(a-1, b, lineAbove) {
-					adjacentSymbol = true
-				} else if charToRight && CheckRange(a, b+1, lineAbove) {
-					adjacentSymbol = true
-				} else if CheckRange(a, b, lineAbove) {
-					adjacentSymbol = true
+				if charToLeft && charToRight {
+					adjacent, symbol, location := CheckRange(a-1, b+1, row-1, lineAbove)
+					if adjacent {
+						adjacentSymbol = true
+						if symbol == "*" {
+							gearTable[location] = append(gearTable[location], numStr)
+						}
+					}
+				} else if charToLeft {
+					adjacent, symbol, location := CheckRange(a-1, b, row-1, lineAbove)
+					if adjacent {
+						adjacentSymbol = true
+						if symbol == "*" {
+							gearTable[location] = append(gearTable[location], numStr)
+						}
+					}
+				} else if charToRight {
+					adjacent, symbol, location := CheckRange(a, b+1, row-1, lineAbove)
+					if adjacent {
+						adjacentSymbol = true
+						if symbol == "*" {
+							gearTable[location] = append(gearTable[location], numStr)
+						}
+					}
+				} else {
+					adjacent, symbol, location := CheckRange(a, b, row-1, lineAbove)
+					if adjacent {
+						adjacentSymbol = true
+						if symbol == "*" {
+							gearTable[location] = append(gearTable[location], numStr)
+						}
+					}
 				}
 			}
 			// check below
 			if charBelow {
 				lineBelow := schematic[row+1]
 
-				if charToLeft && charToRight && CheckRange(a-1, b+1, lineBelow) {
-					adjacentSymbol = true
-				} else if charToLeft && CheckRange(a-1, b, lineBelow) {
-					adjacentSymbol = true
-				} else if charToRight && CheckRange(a, b+1, lineBelow) {
-					adjacentSymbol = true
-				} else if CheckRange(a, b, lineBelow) {
-					adjacentSymbol = true
+				if charToLeft && charToRight {
+					adjacent, symbol, location := CheckRange(a-1, b+1, row+1, lineBelow)
+					if adjacent {
+						adjacentSymbol = true
+						if symbol == "*" {
+							gearTable[location] = append(gearTable[location], numStr)
+						}
+					}
+				} else if charToLeft {
+					adjacent, symbol, location := CheckRange(a-1, b, row+1, lineBelow)
+					if adjacent {
+						adjacentSymbol = true
+						if symbol == "*" {
+							gearTable[location] = append(gearTable[location], numStr)
+						}
+					}
+				} else if charToRight {
+					adjacent, symbol, location := CheckRange(a, b+1, row+1, lineBelow)
+					if adjacent {
+						adjacentSymbol = true
+						if symbol == "*" {
+							gearTable[location] = append(gearTable[location], numStr)
+						}
+					}
+				} else {
+					adjacent, symbol, location := CheckRange(a, b, row+1, lineBelow)
+					if adjacent {
+						adjacentSymbol = true
+						if symbol == "*" {
+							gearTable[location] = append(gearTable[location], numStr)
+						}
+					}
 				}
 			}
 			if adjacentSymbol {
@@ -135,4 +188,21 @@ func main() {
 		sumPartOne += num
 	}
 	fmt.Printf("Sum of numbers with adjacent symbol: %v\n", sumPartOne)
+
+	sumPartTwo := 0
+	for _, val := range gearTable {
+		if len(val) > 1 {
+			product := 1
+			for _, part := range val {
+				partInt, err := strconv.Atoi(part)
+				if err != nil {
+					panic(err)
+				}
+				product *= partInt
+			}
+			sumPartTwo += product
+			// fmt.Printf("%v: %v\n", gear, val)
+		}
+	}
+	fmt.Printf("Sum of gear ratios: %v\n", sumPartTwo)
 }
