692b153696b4d20aae1170fdacbaccc11a18ad06
Joseph Hastings <jwhastings21@gmail.com>
2024-01-01 14:58:51 -0500

    solution for part 2 (finally)

--- 2023/day05/solution.go
+++ 2023/day05/solution.go
@@ -8,14 +8,37 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"slices"
 	"strconv"
 	"strings"
 )
 
-type destination struct {
-	group   string
-	data    []string
-	mapping map[int]int
+// data structure to hold intervals
+type interval struct {
+	interval []int
+}
+
+// CreateInterval creates an interval from a start (a) and end (b) position
+func CreateInterval(a, b int) interval {
+	return interval{[]int{a, b}}
+}
+
+// a accesses the lower end of an interval
+func (i interval) a() int {
+	return i.interval[0]
+}
+
+// b accesses the upper end of an interval
+func (i interval) b() int {
+	return i.interval[1]
+}
+
+// data structure to hold mapping information from one source to the next
+type mapping struct {
+	group       string
+	data        []string
+	source      []interval
+	destination []interval
 }
 
 // StrToIntSlice converts a string containing integers delimited by a space into an integer slice
@@ -33,82 +56,75 @@ func StrToIntSlice(str string) []int {
 	return intSlice
 }
 
-// AddDataToMap adds key-value pairs from its data if the key falls within the specified data
-// Update key to be a range of values (slice) instead of a single int
-func AddDataToMap(keyRange []int, d destination) destination {
-	d.mapping = make(map[int]int)
-	for _, input := range d.data {
-		intSlice := StrToIntSlice(input)
-		destinationStart := intSlice[0]
-		sourceStart := intSlice[1]
-		rangeLength := intSlice[2]
-		sourceRange := []int{sourceStart, sourceStart + rangeLength - 1}
-		// given key range falls within data range
-		// if key >= sourceStart && key <= sourceStart+rangeLength {
-		if RangeOverlap(keyRange, sourceRange) {
-			intersection := RangeIntersect(keyRange, sourceRange)
-			keysOverlapping := RangeSequence(intersection)
-			mapping := []int{keyRange[0] - sourceRange[0] + destinationStart, keyRange[1] - source}
-			// for _, key := range keysOverlapping {
-			// 	d.mapping[key] = key - sourceStart + destinationStart
-			// }
-			// return d // mapping exists in data
+// GetDestination returns destination intervals for the next mapping
+func GetDestination(keyIntervals []interval, m mapping) []interval {
+	// while we have key intervals to check against the mapping data
+	for len(keyIntervals) > 0 {
+		found := false         // initialize a matching key mapping to false
+		key := keyIntervals[0] // grab our first key interval
+		for _, input := range m.data {
+			intSlice := StrToIntSlice(input)
+			mappingStart := intSlice[0] // start of the mapping interval
+			sourceStart := intSlice[1]  // start of the source interval
+			diff := sourceStart - mappingStart
+			rangeLength := intSlice[2]
+			source := CreateInterval(sourceStart, sourceStart+rangeLength-1)
+			// given key falls within data range
+			if IntervalOverlap(key, source) {
+				intersection := IntervalIntersect(key, source)
+				// intersection is a subset of the key
+				if key.a() != intersection.a() || key.b() != intersection.b() {
+					// lower end of key is below start of intersection
+					if key.a() < intersection.a() {
+						keyLower := CreateInterval(key.a(), intersection.a()-1)
+						keyIntervals = append(keyIntervals, keyLower)
+					}
+					// upper end of key is above end of intersection
+					if key.b() > intersection.b() {
+						keyUpper := CreateInterval(intersection.b()+1, key.b())
+						keyIntervals = append(keyIntervals, keyUpper)
+					}
+				}
+				mapping := CreateInterval(intersection.a()-diff, intersection.b()-diff)
+				m.source = append(m.source, intersection)
+				m.destination = append(m.destination, mapping)
+				// key mapping found, remove from the ones we need to check
+				found = true
+				keyIntervals = slices.Delete(keyIntervals, 0, 1)
+				break
+			}
+		}
+		// iterated through all mapping data without a match
+		if !found {
+			m.source = append(m.source, key)
+			m.destination = append(m.destination, key)
+			keyIntervals = slices.Delete(keyIntervals, 0, 1)
 		}
 	}
-	return d // mapping does not exist in data
+	return m.destination
 }
 
-// RangeOverlap takes two integer slices with two elements containing the start and end
+// IntervalOverlap takes two integer slices with two elements containing the start and end
 // of a range (both inclusive) and returns whether they overlap
-func RangeOverlap(x, y []int) bool {
-	return x[0] <= y[1] && y[0] <= x[1]
+func IntervalOverlap(x, y interval) bool {
+	return x.a() <= y.b() && y.a() <= x.b()
 }
 
-// RangeIntersect takes two integer slices with two elements containing the start and end
+// IntervalIntersect takes two integer slices with two elements containing the start and end
 // of a range (both inclusive) and returns a new range of their intersection
-func RangeIntersect(x, y []int) []int {
-	if RangeOverlap(x, y) {
-		return []int{
-			max(x[0], y[0]),
-			min(x[1], y[1]),
+func IntervalIntersect(x, y interval) interval {
+	if IntervalOverlap(x, y) {
+		return interval{[]int{
+			max(x.a(), y.a()),
+			min(x.b(), y.b())},
 		}
 	}
-	return []int{}
-}
-
-// RangeSequence takes an integer slice with two elements containing the start and end of
-// a range (both inclusive) and returns a new slice containing all integers within the range
-func RangeSequence(x []int) (seq []int) {
-	seq = make([]int, x[1]-x[0]+1)
-	i := 0
-	for j := x[0]; j < x[1]+1; j++ {
-		seq[i] = j
-		i++
-	}
-	return seq
-}
-
-// RetrieveDestination returns the value for a given key and map with a destination struct.
-// If the key doesn't exist in the map, the key itself is returned
-func RetrieveDestination(keyRange []int, d destination) int {
-	// value, exists := d.mapping[key]
-	// if exists {
-	// 	return value
-	// }
-	// return key
-	return 1 // temporary for debugging
-}
-
-// AddAndRetrieve
-func AddAndRetrieve(keyRange []int, d destination) int {
-	d = AddDataToMap(keyRange, d)
-	value := RetrieveDestination(keyRange, d)
-	return value
+	return interval{}
 }
 
 func main() {
-	// filename := "input.txt"
-	filename := "input_test.txt"
+	filename := "input.txt"
+	// filename := "input_test.txt"
 	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(err)
@@ -117,13 +133,13 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	var seedString, group string
-	seedToSoil := destination{}
-	soilToFertilizer := destination{}
-	fertilizerToWater := destination{}
-	waterToLight := destination{}
-	lightToTemperature := destination{}
-	temperatureToHumidity := destination{}
-	humidityToLocation := destination{}
+	seedToSoil := mapping{}
+	soilToFertilizer := mapping{}
+	fertilizerToWater := mapping{}
+	waterToLight := mapping{}
+	lightToTemperature := mapping{}
+	temperatureToHumidity := mapping{}
+	humidityToLocation := mapping{}
 
 	// input processing
 	reGroup := regexp.MustCompile(`\w+\-to\-\w+ map\:`)
@@ -172,54 +188,52 @@ func main() {
 	}
 
 	seedInit := StrToIntSlice(seedString)
-	// seedLocation := make([]int, len(seedInit))
-	// for i, seed := range seedInit {
-	// 	soil := AddAndRetrieve([]int{seed, seed}, seedToSoil)
-	// 	fertilizer := AddAndRetrieve([]int{soil, soil}, soilToFertilizer)
-	// 	water := AddAndRetrieve([]int{fertilizer, fertilizer}, fertilizerToWater)
-	// 	light := AddAndRetrieve([]int{water, water}, waterToLight)
-	// 	temperature := AddAndRetrieve([]int{light, light}, lightToTemperature)
-	// 	humidity := AddAndRetrieve([]int{temperature, temperature}, temperatureToHumidity)
-	// 	location := AddAndRetrieve([]int{humidity, humidity}, humidityToLocation)
-	// 	seedLocation[i] = location
-	// }
+	seeds := make([]interval, len(seedInit))
+	for i, seed := range seedInit {
+		seeds[i] = CreateInterval(seed, seed)
+	}
+	soil := GetDestination(seeds, seedToSoil)
+	fertilizer := GetDestination(soil, soilToFertilizer)
+	water := GetDestination(fertilizer, fertilizerToWater)
+	light := GetDestination(water, waterToLight)
+	temperature := GetDestination(light, lightToTemperature)
+	humidity := GetDestination(temperature, temperatureToHumidity)
+	locations := GetDestination(humidity, humidityToLocation)
 
-	// var partOne int
-	// for i := range seedLocation {
-	// 	if i == 1 {
-	// 		partOne = seedLocation[i] // default nearest location to first location
-	// 	} else if seedLocation[i] < partOne {
-	// 		partOne = seedLocation[i] // assign new nearest location
-	// 	}
-	// }
-	// fmt.Printf("Nearest location (part 1): %v\n", partOne)
+	var partOne int
+	for i, location := range locations {
+		if i == 0 {
+			partOne = location.a() // default nearest location to first location
+		} else if location.a() < partOne {
+			partOne = location.a() // assign new nearest location
+		}
+	}
+	fmt.Printf("Nearest location (part 1): %v\n", partOne)
 
-	var seedRanges [][]int
-	for i := 0; i < len(seedInit)/2+1; i += 2 {
+	var seedspt2 = make([]interval, len(seedInit)/2)
+	j := 0
+	for i := 0; i < len(seedInit); i += 2 {
 		start := seedInit[i]
 		length := seedInit[i+1]
-		seedRanges = append(seedRanges, []int{start, start + length - 1})
+		seedspt2[j] = CreateInterval(start, start+length-1)
+		j++
 	}
 
-	// newSeedLocation := make([]int, len(seedRanges))
-	for _, seed := range seedRanges {
-		soil := AddAndRetrieve(seed, seedToSoil)
-		fmt.Printf("%v", soil)
-		// fertilizer := AddAndRetrieve(soil, soilToFertilizer)
-		// water := AddAndRetrieve(fertilizer, fertilizerToWater)
-		// light := AddAndRetrieve(water, waterToLight)
-		// temperature := AddAndRetrieve(light, lightToTemperature)
-		// humidity := AddAndRetrieve(temperature, temperatureToHumidity)
-		// location := AddAndRetrieve(humidity, humidityToLocation)
-		// newSeedLocation[i] = location
+	soil = GetDestination(seedspt2, seedToSoil)
+	fertilizer = GetDestination(soil, soilToFertilizer)
+	water = GetDestination(fertilizer, fertilizerToWater)
+	light = GetDestination(water, waterToLight)
+	temperature = GetDestination(light, lightToTemperature)
+	humidity = GetDestination(temperature, temperatureToHumidity)
+	locations = GetDestination(humidity, humidityToLocation)
+
+	var partTwo int
+	for i, location := range locations {
+		if i == 0 {
+			partTwo = location.a() // default nearest location to first location
+		} else if location.a() < partTwo {
+			partTwo = location.a() // assign new nearest location
+		}
 	}
-	// var partTwo int
-	// for i := range newSeedLocation {
-	// 	if i == 1 {
-	// 		partTwo = newSeedLocation[i]
-	// 	} else if newSeedLocation[i] < partTwo {
-	// 		partTwo = newSeedLocation[i]
-	// 	}
-	// }
-	// fmt.Printf("Nearest location (part 2): %v\n", partTwo)
+	fmt.Printf("Nearest location (part 2): %v\n", partTwo)
 }
